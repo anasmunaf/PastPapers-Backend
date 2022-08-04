425b01041b5ce5df10498f7a14d3717263c31a31
nausif_USD <nausif.momin@uniquesoftwaredev.com>
2022-08-05 01:23:03 +0500

    testing 02

--- utils/multer.js
+++ utils/multer.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const path = require("path");
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../upload"));
+    cb(null, "../uploads");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);

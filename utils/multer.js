69f3c659b68761e6848e54ebd1566497eb43c6ec
nausif_USD <nausif.momin@uniquesoftwaredev.com>
2022-08-05 01:39:34 +0500

    path.resolve applied

--- utils/multer.js
+++ utils/multer.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const path = require("path");
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "../uploads");
+    cb(null, path.resolve(__dirname, "../upload"));
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
